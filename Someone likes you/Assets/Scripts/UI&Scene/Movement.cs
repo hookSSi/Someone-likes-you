8ffd53e23c3a391f84edcc5e5e3bebb3d3dfa34e
hookSSi <sounghoo12@naver.com>
2019-08-03 01:56:42 +0900

    1스테이지 레밸 디자인 완료

--- Someone likes you/Assets/Scripts/UI&Scene/Movement.cs
+++ Someone likes you/Assets/Scripts/UI&Scene/Movement.cs
@@ -8,22 +8,22 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public void MoveVertical(GameObject obj, float value, float time = 1, bool moveType = true)
-    {
-        Vector3 origin = obj.gameObject.transform.position;
-        Vector3 dest = new Vector3(origin.x + value, origin.y, origin.z);
-
-        MoveToDest(obj, dest, time, moveType);
-    }
-    public void MoveHorizontal(GameObject obj, float value, float time = 1, bool moveType = true)
+    public static void MoveVertical(GameObject obj, float value, float time = 1, bool moveType = true)
     {
         Vector3 origin = obj.gameObject.transform.position;
         Vector3 dest = new Vector3(origin.x, origin.y + value, origin.z);
 
         MoveToDest(obj, dest, time, moveType);
     }
+    public static void MoveHorizontal(GameObject obj, float value, float time = 1, bool moveType = true)
+    {
+        Vector3 origin = obj.gameObject.transform.position;
+        Vector3 dest = new Vector3(origin.x + value, origin.y, origin.z);
 
-    public void MoveToDest(GameObject obj ,Vector3 dest, float time = 1, bool moveType = true)
+        MoveToDest(obj, dest, time, moveType);
+    }
+
+    public static void MoveToDest(GameObject obj ,Vector3 dest, float time = 1, bool moveType = true)
     {
         if(moveType) // 시간에 따라 천천히 이동
         {
---
+++ Someone likes you/Assets/Scripts/UI&Scene/Truck.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Truck : MonoBehaviour
+{
+    public delegate void Action(GameObject obj, float value, float time = 1, bool moveType = true);
+    public bool _isDone = false;
+    private float _t = 0;
+
+    public float _waitRange; // 기다리는 시간
+    public float _actRange; // 얼마나 작동 할건지
+    public float _distance;
+
+    private void Start() 
+    {
+        StartCoroutine(Wait(_waitRange, Movement.MoveHorizontal));
+    }
+    IEnumerator Wait(float range, Action action)
+    {
+        // 시간이 다되지 않거나 플레이어 일이 다 마치지 않았다면
+        // 계속 기다린다.
+        while(range > this._t || !_isDone)
+        {
+            this._t  += Time.deltaTime;
+            yield return null;
+        }
+        
+        if(action != null && _isDone)
+        {
+            this._t = 0;
+            StartCoroutine(Act(_actRange, action));
+        }
+    }
+
+    IEnumerator Act(float range, Action action)
+    {
+        while(range > this._t)
+        {
+            this._t += Time.deltaTime;
+            action(this.gameObject, _distance, range);
+            yield return null;
+        }
+
+        if(_isDone)
+            Clear();
+    }
+
+    public void Clear()
+    {
+        this._isDone = false;
+        this._t = 0;
+    }
+    public void Done()
+    {
+        this._isDone = true;
+    }
+}
