669e0e3f8f3ae262ac07994f75a33ba8e0eccfe0
aycaramba <rjsxkr1122@naver.com>
2019-08-02 21:05:05 +0900

    Stone here에 돌던지면 문열림(예시)

---
+++ Someone likes you/Assets/Scripts/StoneTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneTrigger : MonoBehaviour
+{
+    public GameObject door;
+
+    public void act()
+    {
+        if (door.activeSelf == true)
+        {
+            door.SetActive(false);
+        }
+        else
+        {
+            door.SetActive(true);
+        }
+    }
+}
