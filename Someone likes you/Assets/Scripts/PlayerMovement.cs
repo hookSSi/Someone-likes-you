5da59f5780a05a71a36e77e0de6c8ebef96be0fb
MINSEO <Nicitis@users.noreply.github.com>
2019-08-03 14:34:33 +0900

    Player와 플랫폼 Scale에 따라 파쿠르에 버그가 생기던 것을 수정했습니다.

--- Someone likes you/Assets/Scripts/PlayerMovement.cs
+++ Someone likes you/Assets/Scripts/PlayerMovement.cs
@@ -12,8 +12,8 @@ public class PlayerMovement : MonoBehaviour
     public float climbSpeed = 2f;
     public float climbJumpPower = 1.8f;
 
-    public float minLengthHangable = 0.4f; // collider 가장 위에서부터 아래로 길이 - 머리 크기인 20 픽셀 * 2(올라탈 수 있는 범위)(1 픽셀 : 0.01)
-    public float maxLengthHangable = 0.2f; // collider 가장 위에서부터 위로의 길이
+    public float minLengthHangable = 0.4f; // collider 가장 위에서부터 아래로 길이(비율) - 머리 크기인 20 픽셀 * 2(올라탈 수 있는 범위)(1 픽셀 : 0.01)
+    public float maxLengthHangable = 0.2f; // collider 가장 위에서부터 위로의 길이(비율)
 
     private bool isJumping = false;
     public bool isGround = false;
@@ -86,12 +86,12 @@ public class PlayerMovement : MonoBehaviour
             if (Input.GetAxisRaw("Horizontal") < 0)
             {
                 Move(Vector3.left);
-                transform.localScale = new Vector3(-1f, 1f, 1f); // 강제로 스프라이트를 뒤집는 방법
+                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z); // 강제로 스프라이트를 뒤집는 방법
             }
             else if (Input.GetAxisRaw("Horizontal") > 0)
             {
                 Move(Vector3.right);
-                transform.localScale = new Vector3(1f, 1f, 1f);
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
             Jump();
             _animator.SetFloat("speed", Mathf.Abs(rigid.velocity.x));
@@ -145,26 +145,26 @@ public class PlayerMovement : MonoBehaviour
 
     IEnumerator Climb(float deltaHeight, bool isRight) // 파쿠르 강제 올라가기
     {
-        // 현재 문제점 : Scale을 강제로 조절한 플랫폼은 파쿠르가 이상하게 작동하는 버그가 있습니다.
+        // Scale을 강제로 조절한 플랫폼은 파쿠르가 이상하게 작동하는 버그가 있어서 이를 수정했습니다.
         // - 오브젝트의 Scale을 강제로 조절해도, Collider의 수치 상의 offset과 size는 변하지 않기 때문!
-        // - 해결법 1. size에 scale을 강제로 곱하여 해결한다.
-        // (굉장히 단순하지만 효과적인 방법, 하지만 상당히 귀찮고 코드를 알아보기 힘들 것 같습니다.)(아직 미적용)
+        // - size에 scale을 강제로 곱하여 해결하였으며, 때문에 Collider의 size 또는 offset에 관련된 수에 localScale.x 또는 localScale.y가 곱해졌습니다.
         Vector3 startPosition = transform.position;
+        Vector3 dir;
         float time = 0;
         float _moveSpeed;
-        float distance = deltaHeight + ((BoxCollider2D)playerCollider).size.y / 2; // 올라가야 하는 거리
+        float distance = deltaHeight + ((BoxCollider2D)playerCollider).size.y * transform.localScale.y / 2; // 올라가야 하는 거리
         float timeExpected = distance / climbSpeed;
 
+        if(isRight) dir = Vector3.right;
+        else dir = Vector3.left;
         isClimbing = true;
         rigid.simulated = false;
         rigid.velocity = Vector2.zero;
 
         while (true) // y 좌표 이동
         {
-            transform.position += Vector3.up * (climbSpeed * Time.fixedDeltaTime);
-
             yield return new WaitForFixedUpdate();
-
+            transform.position += Vector3.up * (climbSpeed * Time.fixedDeltaTime);
             time += Time.fixedDeltaTime;
 
             if (time >= timeExpected) // 어느 정도의 시간이 흐르면
@@ -174,9 +174,8 @@ public class PlayerMovement : MonoBehaviour
         }
 
         time = 0;
-        distance = ((BoxCollider2D)playerCollider).size.x; // 플레이어 사이즈 만큼 x축으로 이동
+        distance = ((BoxCollider2D)playerCollider).size.x * transform.localScale.x; // 플레이어 사이즈 만큼 x축으로 이동
 
-        if (!isRight) distance *= -1;
 
         yield return new WaitForFixedUpdate();
         rigid.simulated = true;
@@ -193,9 +192,9 @@ public class PlayerMovement : MonoBehaviour
             if (Input.GetAxisRaw("Horizontal") != 0)
                 break;
 
-            transform.position += Vector3.right * _moveSpeed * Time.fixedDeltaTime;
-
             yield return new WaitForFixedUpdate();
+            
+            transform.position += Vector3.right * _moveSpeed * Time.fixedDeltaTime;
 
             time += Time.fixedDeltaTime;
 
@@ -210,14 +209,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision) // 벽 타고 오르기
     {
+        // localScale.y가 마이너스가 되면 대참사가 벌어질 수도 있으니 주의....
         if (collision.collider.tag == "Ground"/*&& collision.transform.position.y < transform.position.y*/)
         {
             if (!isGround)
             {
-                float colHeight = ((BoxCollider2D)collision.collider).size.y;
-                float deltaHeight = collision.transform.position.y + colHeight / 2f - (playerCollider.offset.y + playerCollider.transform.position.y);
-                float playerHeight = ((BoxCollider2D)playerCollider).size.y;
-                if (deltaHeight <= playerHeight / 2f + maxLengthHangable && deltaHeight >= playerHeight / 2f - minLengthHangable) // 올라탈 수 있는 가능 범위
+                Vector3 colScale = collision.transform.localScale;
+                float colHeight = ((BoxCollider2D)collision.collider).size.y * colScale.y;
+                float deltaHeight = collision.transform.position.y + colHeight / 2f - (playerCollider.offset.y * transform.localScale.y + transform.position.y);
+                float playerHeight = ((BoxCollider2D)playerCollider).size.y * transform.localScale.y;
+                if (deltaHeight <= playerHeight / 2f + maxLengthHangable * playerHeight && deltaHeight >= playerHeight / 2f - minLengthHangable * playerHeight) // 올라탈 수 있는 가능 범위
                 {
                     StartCoroutine(Climb(deltaHeight, collision.transform.position.x > transform.position.x));
                 }
