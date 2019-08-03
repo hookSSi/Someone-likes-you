743d2a1d6fbf00d67d1fe462a6f216ab564ea76c
MINSEO <Nicitis@users.noreply.github.com>
2019-08-04 01:00:06 +0900

    움직이는 플랫폼을 만들었습니다. Dir 의방향을 수정하면 좌우가 아닌 다른 방향으로도 이동하며, 플랫폼에 Moving Platform을 넣고 끝나는 지점의 tag를 Ground로 한 뒤 trigger를 붙여주시면 됩니다.

--- Someone likes you/Assets/Scripts/MovingPlatform.cs
+++ Someone likes you/Assets/Scripts/MovingPlatform.cs
@@ -4,43 +4,93 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    // Ground 태그 오브젝트와 충돌 시 방향 변환, PlatformPath 태그 오브젝트의 Trigger에 진입 시 방향 전환
     public float speed = 2f;
     public float idleTime = 2.4f;
-    public bool isActivated;
-    public bool directionByCollision = false; // 충돌에 따라 방향이 바뀌는가?(true -> 목적지 존재, false -> 충돌할 때까지 전진)
 
-    public Vector3 startPos;
-    public Vector3 endPos;
     public Vector3 dir = Vector3.right; // dir를 Up-Down 방식 또는 Left-Right로 바꾸면 어디에든 적용할 수 있다.
     public Rigidbody2D rigid;
+    public State state;
+    
+    public enum State
+    {
+        Deactivated, Idle, Activated
+    }
 
     private void Awake()
     {
+        state = State.Activated;
         rigid = GetComponent<Rigidbody2D>();
         if(!rigid)
         {
             rigid = gameObject.AddComponent<Rigidbody2D>();
         }
+        rigid.bodyType = RigidbodyType2D.Kinematic;
     }
 
     private void FixedUpdate()
     {
-        if(isActivated)
+        switch(state)
         {
-            Move();
+            case State.Activated:
+                Move();
+                break;
+
+            case State.Idle:
+                break;
+
+            case State.Deactivated:
+                break;
         }
     }
 
     private void Move()
     {
-        rigid.velocity = dir * speed * Time.fixedDeltaTime; // fixed? original? 뭘 써야 할까?
+        transform.position += dir * speed * Time.fixedDeltaTime; // fixed? original? 뭘 써야 할까?
     }
 
-    void OnCollisionEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag != "Player" && directionByCollision)
+        if(collision.tag == "PlatformPath" || collision.tag == "Ground")
         {
+            ChangeDirection();
+        }
+    }
+    
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            // Attach
+            collision.transform.parent = transform;
+        }
+    }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && collision.transform.parent == transform)
+        {
+            collision.transform.parent = null;
+            if (state == State.Activated)
+               collision.GetComponent<Rigidbody2D>().velocity += (Vector2)dir * speed;
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        dir *= -1;
+        StartCoroutine("WaitIdleTime");
+    }
+
+    private IEnumerator WaitIdleTime()
+    {
+        if (state != State.Activated) yield break;
+        state = State.Idle;
+        // Wait Idle Time();
+        yield return new WaitForSeconds(idleTime);
+        if(state == State.Idle)
+        {
+            state = State.Activated;
         }
     }
 }
